905173922e56e58c0bded5c8f6c49f451921736a
SewerynJagodzinski <sewerynjagodzinski5@gmail.com>
2025-06-10 23:10:44 +0200

    First version of app, added basic functionality

---
+++ main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include "Registry.h"
+
+int main() {
+    Registry registry;
+    int choice;
+
+    while (true) {
+        std::cout << "\n=== REJESTR SAMOCHODOW ===\n";
+        std::cout << "1. Dodaj auto\n";
+        std::cout << "2. Usun auto\n";
+        std::cout << "3. Edytuj auto\n";
+        std::cout << "4. Wyswietl wszystkie auta\n";
+        std::cout << "5. Wyjscie\n";
+        std::cout << "Wybor: ";
+        std::cin >> choice;
+
+        switch (choice) {
+            case 1: {
+                std::string brand;
+                int year, mileage;
+                std::cout << "Marka: ";
+                std::cin >> brand;
+                std::cout << "Rok zakupu: ";
+                std::cin >> year;
+                std::cout << "Przebieg [km]: ";
+                std::cin >> mileage;
+                registry.addCar(brand, year, mileage);
+                break;
+            }
+            case 2: {
+                int index;
+                std::cout << "Indeks auta do usuniecia: ";
+                std::cin >> index;
+                registry.deleteCar(index);
+                break;
+            }
+            case 3: {
+                int index;
+                std::string brand;
+                int year, mileage;
+                std::cout << "Indeks auta do edycji: ";
+                std::cin >> index;
+                std::cout << "Nowa marka: ";
+                std::cin >> brand;
+                std::cout << "Nowy rok zakupu: ";
+                std::cin >> year;
+                std::cout << "Nowy przebieg: ";
+                std::cin >> mileage;
+                registry.editCar(index, brand, year, mileage);
+                break;
+            }
+            case 4:
+                registry.displayAll();
+                break;
+            case 5:
+                std::cout << "Zamykanie programu...\n";
+                return 0;
+            default:
+                std::cout << "Nieprawidlowy wybor!\n";
+        }
+    }
+
+    return 0;
+}
